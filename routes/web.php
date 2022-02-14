f5c12abf57b3bc850e664f1f775b5c97373e5ff2
Tyler W <tyler@tylerw.dev>
2022-02-14 06:00:33 +0000

    fisrt commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+  return view('posts', [
+    'posts' => Post::latest()->with('category', 'author')->get()
+  ]);
+});
+
+Route::get('posts/{post:slug}', function (Post $post) {
+  return view('post', [
+    'post' => $post
+  ]);
+});
+
+Route::get('categories/{category:slug}', function (Category $category) {
+  return view('posts', [
+    'posts' => $category->posts->load('category', 'author')
+  ]);
+});
+
+Route::get('authors/{author:username}', function (User $author) {
+  return view('posts', [
+    'posts' => $author->posts->load('category', 'author')
+  ]);
+});

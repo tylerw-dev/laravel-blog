5d793813be085852c7b87dccd299a8b2770bdd78
Tyler Wasson <tyler.wasson@oyova.com>
2022-03-06 08:31:38 -0600

    added search functionality to homepage

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,6 +11,14 @@ class Post extends Model
 
     protected $guarded = [];
 
+    public function scopeFilter($query, array $filters) {
+      $query->when($filters['search'] ?? false, function ( $query, $search ) {
+        $query
+          ->where('title', 'like', '%' . $search . '%')
+          ->orWhere('body', 'like', '%' . $search . '%');
+      });
+    }
+
     public function category() {
       return $this->belongsTo(Category::class);
     }
